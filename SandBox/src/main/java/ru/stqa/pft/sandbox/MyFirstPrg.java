5c3ccfa236411c830128a705ba527c304ccd9717
pernatyzmei <loilek@yandex.ru>
2021-04-19 01:42:52 +0300

    Задание  №2: Потренироваться использовать функции, классы, объекты и методы

--- SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstPrg.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstPrg.java
@@ -3,16 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyFirstPrg {
 	
 	public static void main(String[] args){
-		Point a = new Point();
+		// Пример вызова метода 1
+		Point a = new Point(1,0);
+		Point b = new Point(4,0);
+		System.out.println("Пример 1" +  "\n" + "Расстояние между точками a и b = "+Point.distance(a, b));
+
+		// Пример вызова метода 2
 		a.x = 1;
-		a.y = 0;
-		Point b = new Point();
+		a.y = 4;
 		b.x = 3;
-		b.y = 0;
-		System.out.println(distance(a, b));
+		b.y = 8;
+		System.out.println("Пример 2" +  "\n" +"Расстояние между точками a и b = "+Point.distance(a, b));
+
+		// Пример вызова метода 3
+		a.x = 0.5;
+		a.y = 6;
+		b.x = 2.2;
+		b.y = 4;
+		System.out.println("Пример 3" +  "\n" +"Расстояние между точками a и b = "+Point.distance(a, b));
 	}
-	
-	public static String distance(Point p1, Point p2){
-		return ("Расстояние между точками р1 и р2 = " +  Math.sqrt((p2.x- p1.x)*(p2.x- p1.x)+(p2.y- p1.y)*(p2.y- p1.y)));
-	}
+
+
+
 }
--- SandBox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,5 +3,16 @@ package ru.stqa.pft.sandbox;
 public class Point {
   public double x;
   public double y;
+
+  Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+
+  static double distance(Point p1, Point p2){
+    return (Math.sqrt((p2.x- p1.x)*(p2.x- p1.x)+(p2.y- p1.y)*(p2.y- p1.y)));
+  }
+
 }
 
