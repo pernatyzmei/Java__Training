cceca1b23d0ebff4727e7b421b302209643d7a97
pernatyzmei <loilek@yandex.ru>
2021-04-19 01:15:39 +0300

    Создана функция distance

--- SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstPrg.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/MyFirstPrg.java
@@ -3,7 +3,16 @@ package ru.stqa.pft.sandbox;
 public class MyFirstPrg {
 	
 	public static void main(String[] args){
-		
-		System.out.println("Hello, World!");
+		Point a = new Point();
+		a.x = 1;
+		a.y = 0;
+		Point b = new Point();
+		b.x = 3;
+		b.y = 0;
+		System.out.println(distance(a, b));
+	}
+	
+	public static String distance(Point p1, Point p2){
+		return ("Расстояние между точками р1 и р2 = " +  Math.sqrt((p2.x- p1.x)*(p2.x- p1.x)+(p2.y- p1.y)*(p2.y- p1.y)));
 	}
 }
---
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,7 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public double x;
+  public double y;
+}
+
