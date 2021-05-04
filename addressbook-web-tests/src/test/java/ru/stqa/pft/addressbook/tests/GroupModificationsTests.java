418138a96e736a6494168940ead9d8dc0e8b5f7a
pernatyzmei <loilek@yandex.ru>
2021-05-05 01:39:10 +0300

    Update GroupModificationsTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationsTests.java
@@ -8,6 +8,9 @@ public class GroupModificationsTests extends TestBase{
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("Group1", "group_header_1", "group_footer_1"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Group1", "group_header_1", "group_footer_1"));
