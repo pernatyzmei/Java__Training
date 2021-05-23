0fe7ba921a17b5853b8e8dd89d32e34cd57f3a4c
pernatyzmei <loilek@yandex.ru>
2021-05-23 20:23:24 +0300

    Лекция 6.3. Анализ опций командой строки при запуске генератора данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -10,17 +13,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
+
+  @Parameter(names= "-c", description = "Contact count")
+  public int count;
+
+  @Parameter(names= "-f", description = "Target file")
+  public String file;
+
   public static void main(String[] args) throws IOException {
 
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-
-    List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
 
   }
 
-  private static List<ContactData> generateContacts(int count) {
+  private void run ()  throws IOException{
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+
+  private  List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
       contacts.add(new ContactData().withFirstname(String.format("Test Firstname %s", i)).
@@ -29,15 +49,15 @@ public class ContactDataGenerator {
               withAddress(String.format("Test Address %s", i)).withHomePhone(String.format("1-23-4%s", i)).
               withMobilePhone(String.format("8(911)110-67-5%s", i)).withWorkPhone(String.format("2 95 8%s", i)).
               withFirstMail(String.format("TestFirst@Mail.ru %s", i)).withSecondMail(String.format("TestSecond@Mail.ru %s", i)).
-              withThirdMail(String.format("TestThirdMail.ru %s", i)));
+              withThirdMail(String.format("TestThird@Mail.ru %s", i)));
     }
     return contacts;
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void save(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact:contacts){
-      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getMiddlename(), contact.getLastname(),
+      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getMiddlename(), contact.getLastname(),
               contact.getNickname(), contact.getCompany(), contact.getAddress(), contact.getHomePhone(), contact.getMobilePhone(),
               contact.getWorkPhone(), contact.getFirstMail(), contact.getSecondMail(), contact.getThirdMail()));
     }
